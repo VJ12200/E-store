5a3a37cf14ad2098e6b2b6d1c4857c69985cbb6a
VJ1201 <126790414+VJ12200@users.noreply.github.com>
2025-09-06 20:18:10 +0530

    Remove route for top selling products

--- backend/routes/analyticsRoutes.js
+++ backend/routes/analyticsRoutes.js
@@ -1,9 +1,8 @@
 import express from 'express';
-import { getTopSellingProducts, getCategoryStats } from '../controllers/analyticsController.js';
+import { getCategoryStats } from '../controllers/analyticsController.js';
 
 const router = express.Router();
 
-router.get('/top-selling', getTopSellingProducts);
 router.get('/categories', getCategoryStats);
 
 export default router;
